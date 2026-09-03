8ba5425b6051f8e1f09bd848f3ef44beb6353abc
David Bond <david.bond@panoramicdata.com>
2026-09-03 13:37:27 +0100

    chore: remove unused TestHandler class from unit tests

--- TeamViewer.Api.Test/UnitTests/AuthenticationHandlerTests.TestHandler.cs
+++
@@ -1,7 +0,0 @@
-namespace TeamViewer.Api.Test.UnitTests;
-
-internal class TestHandler(Func<HttpRequestMessage, CancellationToken, Task<HttpResponseMessage>> handler) : HttpMessageHandler
-{
-	protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
-		=> handler(request, cancellationToken);
-}
